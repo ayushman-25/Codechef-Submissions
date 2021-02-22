c9e09f81ce81f8a8b24a553151f84aaf5ecd0689
ayushman-25 <ayushman25@gmail.com>
2021-02-22 09:53:46 +0530

    New

--- CCBTS01.py
+++ CCBTS01.py
@@ -75,95 +75,10 @@ readarrs = lambda: [str(_) for _ in sys.stdin.readline().rstrip("\r\n").split()]
 mod = 998244353
 MOD = int(1e9) + 7
 
-N = int(1e5) + 1
-sys.setrecursionlimit(N)
-distances = [0] * N
-height, euler, first, segtree, visited = [], [], [], [], []
-n = None
-
-
-def clear_global():
-    global height, euler, first, segtree, visited, n
-    height.clear()
-    euler.clear()
-    first.clear()
-    segtree.clear()
-    visited.clear()
-
-
-def dfs(adj, node, h=0):
-    global visited, height, euler, distances
-    visited[node] = 1
-    height[node] = h
-    first[node] = len(euler)
-    euler.append(node)
-    for i in adj[node]:
-        if(not (visited[i[0]])):
-            distances[i[0]] = distances[node] + i[1]
-            dfs(adj, i[0], h + 1)
-            euler.append(node)
-
-
-def build(node, b, e):
-    global segtree, euler, height
-    if (b == e): segtree[node] = euler[b]
-    else:
-        mid = (b + e) >> 1
-        build(node << 1, b, mid)
-        build(node << 1 | 1, mid + 1, e)
-        l = segtree[node << 1]
-        r = segtree[node << 1 | 1]
-        segtree[node] = l if (height[l] < height[r]) else r
-
-
-def query(node, b, e, L, R):
-    global segtree, height
-    if (b > R or e < L): return -1
-    if (b >= L and e <= R): return segtree[node]
-    mid = (b + e) >> 1
-    left = query(node << 1, b, mid, L, R)
-    right = query(node << 1 | 1, mid + 1, e, L, R)
-    if (left == -1): return right
-    if (right == -1): return left
-    return left if (height[left] < height[right]) else right
-
-
-def preC(adj, root=0):
-    global height, euler, first, segtree, visited, n
-    n = len(adj)
-    height = [0 for _ in range(n)]
-    first = [0 for _ in range(n)]
-    visited = [0 for _ in range(n)]
-    dfs(adj, root)
-    m = len(euler)
-    segtree = [0 for _ in range(m * 4)]
-    build(1, 0, m - 1)
-
-
-def lca(u, v):
-    global first, euler
-    left, right = first[u], first[v]
-    right = first[v]
-    if (left > right): left, right = right, left
-    return query(1, 0, len(euler) - 1, left, right)
-
 
 def solve():
-    global distances
-    n, q, R = readints()
-    adj = [[] for _ in range(n)]
-    distances = [0 for _ in range(n + 1)]
-    for _ in range(n - 1):
-        l, r, dist = readints()
-        adj[l - 1].append([r - 1, dist])
-        adj[r - 1].append([l - 1, dist])
-    preC(adj)
-    for _ in range(q):
-        u, v = readints()
-        u -= 1; v -= 1
-        LCA = lca(u, v)
-        print(distances[u] + distances[v] - distances[LCA])
-    clear_global()
+    s = readstr()
+    print("YES" if len(set(s)) == 3 else "NO")
 
 
 def main():
--- temp2.cpp
+++ temp2.cpp
@@ -1,40 +1,81 @@
-/*
-
-*  Author : Ayushman Chahar   #
-*  About  : IT Sophomore      #
-*  Insti  : VIT, Vellore      #
-
-*/
-
-#pragma GCC optimize("Ofast")  
-#pragma GCC target("avx,avx2,fma") 
-#pragma GCC optimization ("unroll-loops")
-
 #include<bits/stdc++.h>
 using namespace std;
+#define all(v) (v).begin(),(v).end()
+#define int long long int
 
-#define MOD 1000000007
-#define mod 998244353
+vector<vector<pair<int,int> > > graph;
+vector<int> lvl,first,euler,dis,tree;
+vector<bool> vis;
+int n,q,r;
 
-typedef long long int ll;
-typedef unsigned long long int ull;
-
-template<typename T, typename U> inline void amin(T &x, U y) {if(y < x) x = y;}
-template<typename T, typename U> inline void amax(T &x, U y) {if(x < y) x = y;}
-
-void solve() {
-    
-
+void init(){
+    euler.clear();
+    cin>>n>>q>>r;
+    graph.assign(n+1,{});
+    lvl.assign(n+1,0);
+    dis.assign(n+1,-1);
+    first.assign(n+1,-1);
+    vis.assign(n+1,false);
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
-    int t = 1, casee = 1;
-    cin >> t;
-    while(t--) {
-        // cout << "Case #" << casee++ << ": ";
-        solve();
+void build(int l = 0,int r = euler.size()-1,int i = 0){
+    if(l == r) { tree[i] = euler[l]; return; }
+    build(l,l+(r-l)/2,2*i+1);
+    build(l+(r-l)/2+1,r,2*i+2);
+    tree[i] = (lvl[tree[2*i+1]] < lvl[tree[2*i+2]] ? tree[2*i+1] : tree[2*i+2]);
+}
+
+int lca(int ql,int qr,int l = 0,int r = euler.size()-1,int i = 0){
+    if(ql < 0 or qr > euler.size()-1 or qr < ql or ql > r or qr < l) return 0;
+    if(ql <= l and r <= qr) return tree[i];
+    int a = lca(ql,qr,l,l+(r-l)/2,2*i+1), b = lca(ql,qr,l+(r-l)/2+1,r,2*i+2);
+    return (lvl[a] < lvl[b] ? a : b);
+}
+
+void dfs(int src,int d=0){
+    lvl[src] = d;
+    first[src] = euler.size();
+    euler.push_back(src);
+    vis[src] = true;
+    for(auto {child,w}:graph[src]){
+        if(!vis[child]){
+            dis[child] = dis[src] + w;
+            dfs(child,d+1);
+            euler.push_back(src);
+        }
     }
+}
+
+void solve(){
+    init();
+    for(int i=0;i<n-1;i++){
+        int u,v,w; cin>>u>>v>>w;
+        graph[u].push_back({v,w});
+        graph[v].push_back({u,w});
+    }
+    dis[0] = INT_MAX;
+    lvl[0] = INT_MAX;
+    dis[r] = 0;
+    dfs(r);
+    tree.assign(4*euler.size()+4,0);
+    build();
+    while(q--){
+        int u,v; cin>>u>>v;
+        if(first[u] > first[v]) swap(u,v);
+        int x = lca(first[u],first[v]);
+        int ans = dis[u] + dis[v] - 2*dis[x];
+        cout<<ans<<"\n";
+    }
+}
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    // freopen("C:\\input", "r", stdin);
+    // freopen("C:\\output", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int t; cin>>t;
+    while(t--) solve();
     return 0;
 }
